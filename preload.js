4d4d1441fb63d8a06dc86c8670c7d8da197861c7
Jason Chavannes <jason.chavannes@gmail.com>
2021-09-02 07:44:34 -0700

    Move main.js

--- preload.js
+++ preload.js
@@ -3,18 +3,5 @@ const {contextBridge} = require("electron");
 let Versions = {}
 for (const dependency of ['chrome', 'node', 'electron']) {
     Versions[dependency] = process.versions[dependency]
-    //replaceText(`${dependency}-version`, process.versions[dependency])
 }
 contextBridge.exposeInMainWorld("Versions", Versions);
-
-/*window.addEventListener('DOMContentLoaded', () => {
-    const replaceText = (selector, text) => {
-        const element = document.getElementById(selector)
-        if (element) element.innerText = text
-    }
-    window.Versions = {}
-    for (const dependency of ['chrome', 'node', 'electron']) {
-        window.Versions[dependency] = process.versions[dependency]
-        //replaceText(`${dependency}-version`, process.versions[dependency])
-    }
-})*/
