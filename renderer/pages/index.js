45e2ead8cdee73906aa67f252b5648d4849d1af9
Jason Chavannes <jason.chavannes@gmail.com>
2021-09-16 05:12:53 +0000

    Get versions like before

--- renderer/pages/index.js
+++ renderer/pages/index.js
@@ -3,13 +3,19 @@ import LikeButton from "../components/like_button";
 import ConnectForm from "../components/connect_form";
 
 const Home = () => {
-    const [node, chrome, electron] = useState(0);
-    const node2 = process.versions["node"]
+    const [node, setNode] = useState(0);
+    const [chrome, setChrome] = useState(0);
+    const [electron, setElectron] = useState(0);
+    React.useEffect(() => {
+        setNode(window.Versions["node"])
+        setChrome(window.Versions["chrome"])
+        setElectron(window.Versions["electron"])
+    })
     return (
         <div>
             <h1>Memo Desktop!</h1>
             <p>
-                We are using Node.js {node} - {node2},
+                We are using Node.js {node},
                 Chromium {chrome},
                 and Electron {electron}.
             </p>
