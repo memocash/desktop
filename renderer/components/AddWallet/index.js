3ddfb4f8abaf8dcfcec0daf989e2bb0c1877d53b
Jason Chavannes <jason.chavannes@gmail.com>
2021-11-19 14:45:21 -0800

    Start to combine import/create wallet panes

--- renderer/components/AddWallet/index.js
+++ renderer/components/AddWallet/index.js
@@ -5,12 +5,7 @@ const AddWalletHome = ({
     onCreateWallet,
     walletNameInput,
 }) => {
-    const [addWalletMethod, setAddWalletMethod] = useState() // check initial value
     const [fileStatus, setFileStatus] = useState(false)
-
-    const handleChooseAddWallet = (e) => {
-        setAddWalletMethod(e.target.value)
-    };
 
     const onChange = (e) => {
         window.electron.checkFile(e.target.value).then((status) => {
@@ -18,36 +13,15 @@ const AddWalletHome = ({
         })
     }
 
-    const walletOptions = {
-        create: (
+    return (
+        <div>
             <div>
                 <label>Wallet name:
                     <input ref={walletNameInput} onChange={onChange} type="text" />
+                    <button onClick={onClickImport}>Import</button>
                 </label>
                 <p>{fileStatus ? "Exists!" : "Doesn't exist!"}</p>
-                <button onClick={onCreateWallet}>Create</button>
-            </div>
-        ),
-        import: (
-            <div>
-                <button onClick={onClickImport}>Import</button>
-            </div>
-        )
-    }
-
-    return (
-        <div>
-            <h2>How would you like to add this wallet?</h2>
-            <div onChange={handleChooseAddWallet}>
-                <label>Create a new Memo wallet
-                    <input type="radio" name="wallet" value="create" />
-                </label>
-                <label>Import an existing Memo wallet
-                    <input type="radio" name="wallet" value="import" />
-                </label>
-            </div>
-            <div>
-                {walletOptions[addWalletMethod]}
+                <button onClick={onCreateWallet}>Choose...</button>
             </div>
         </div>
     )
