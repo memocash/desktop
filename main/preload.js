d9585ec16da6dace852f94160c42f214d72d54f1
Jason Chavannes <jason.chavannes@gmail.com>
2021-12-01 22:33:25 +0000

    Initial work on encrypting and decrypting file.

--- main/preload.js
+++ main/preload.js
@@ -1,5 +1,5 @@
 const {ipcRenderer, clipboard, contextBridge} = require('electron')
-const myElectron = require('electron')
+const crypto = require("crypto")
 const fs = require("fs/promises")
 const homedir = require('os').homedir()
 
@@ -38,6 +38,24 @@ contextBridge.exposeInMainWorld('electron', {
         }
         if (password) {
             obj.password = password
+            let contents = JSON.stringify(obj)
+            let salt = crypto.randomBytes(128)
+            let derivedKey = crypto.pbkdf2Sync(password, salt, 10000, 32, "sha256")
+            let iv = crypto.randomBytes(16)
+            let cipher = crypto.createCipheriv("aes-256-gcm", derivedKey, iv)
+            let encrypted = cipher.update(Buffer.from(contents, "utf8"), "utf8", "base64")
+            encrypted += cipher.final("base64")
+            let authTag = cipher.getAuthTag()
+            console.log("Encrypted: " + encrypted)
+            //await fs.writeFile(wallet, encrypted)
+
+            let decipher = crypto.createDecipheriv("aes-256-gcm", derivedKey, iv)
+            decipher.setAuthTag(authTag)
+            let decrypted = decipher.update(encrypted, "base64", "utf8")
+            decrypted += decipher.final("utf8")
+            decrypted = Buffer.from(decrypted, "utf8")
+            console.log("decrypted: " + decrypted)
+            // TODO: Save encrypted file and be able to read it
         }
         await fs.writeFile(wallet, JSON.stringify(obj))
     },
