3ea9915f4e913e8ddf20e0a6800e032b7930c061
Jason Chavannes <jason.chavannes@gmail.com>
2022-01-11 21:00:35 +0000

    Verify key list

--- renderer/components/AddWallet/import_keys.js
+++ renderer/components/AddWallet/import_keys.js
@@ -1,11 +1,28 @@
-import {useRef} from "react"
+import {useRef, useState} from "react"
+import {ECPair} from '@bitcoin-dot-com/bitcoincashjs2-lib'
 
 
 const ImportKeys = ({onSetKeys, onBack}) => {
+    const [showError, setShowError] = useState(false)
     const privateKeyList = useRef()
 
     const handleClickNext = () => {
-        onSetKeys(privateKeyList.current.value.split("\n"))
+        const keyList = privateKeyList.current.value.split("\n")
+        for (let i = 0; i < keyList.length; i++) {
+            const wif = keyList[i]
+            try {
+                const address = ECPair.fromWIF(wif).getAddress()
+                if (!address || !address.length) {
+                    setShowError(true)
+                    return
+                }
+            } catch (err) {
+                console.log(err)
+                setShowError(true)
+                return
+            }
+        }
+        onSetKeys(keyList)
     }
 
     return (
@@ -13,7 +30,8 @@ const ImportKeys = ({onSetKeys, onBack}) => {
             <h2>Import Bitcoin Keys</h2>
             <div>
                 <div>Enter a list of Bitcoin private keys.</div>
-                <textarea ref={privateKeyList}/>
+                <textarea onChange={() => setShowError(false)} ref={privateKeyList}/>
+                {showError ? <p>ERROR: Invalid WIF(s) or no WIFs entered</p> : null}
                 <p>
                     <button onClick={handleClickNext}>Next</button>
                 </p>
