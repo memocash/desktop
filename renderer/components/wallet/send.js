1f1005a7f9f929147da7bfef94d40f112c3b8bdd
Jason Chavannes <jason.chavannes@gmail.com>
2022-01-12 21:30:48 +0000

    Initial send form

--- renderer/components/wallet/send.js
+++ renderer/components/wallet/send.js
@@ -1,7 +1,33 @@
+import {useRef} from "react";
+import form from "../../styles/form.module.css"
+
 const Send = () => {
+    const payToRef = useRef()
+    const messageRef = useRef()
+    const amountRef = useRef()
     return (
         <div>
-            <h2>Send</h2>
+            <p>
+                <label>
+                    <span className={form.span}>Pay to:</span>
+                    <input className={form.input} ref={payToRef} type="text" autoFocus/>
+                </label>
+            </p>
+            <p>
+                <label>
+                    <span className={form.span}>Message (optional):</span>
+                    <input className={form.input} ref={messageRef} type="text"/>
+                </label>
+            </p>
+            <p>
+                <label>
+                    <span className={form.span}>Amount:</span>
+                    <input className={form.input_small} ref={amountRef} type="text"/>
+                </label>
+            </p>
+            <p>
+                <button>Preview</button>
+            </p>
         </div>
     )
 }
