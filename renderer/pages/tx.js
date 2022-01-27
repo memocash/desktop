91ba8960e84e1028434bc3350390938f8c368c4e
Jason Chavannes <jason.chavannes@gmail.com>
2022-01-27 21:26:19 +0000

    Display data passed to tx preview window.

--- renderer/pages/tx.js
+++ renderer/pages/tx.js
@@ -1,8 +1,28 @@
+import {useRouter} from "next/router";
+import {useEffect, useState} from "react";
 
 const Tx = () => {
+    const router = useRouter()
+    const [inputPayTo, setInputPayTo] = useState()
+    const [inputMessage, setInputMessage] = useState()
+    const [inputAmount, setInputAmount] = useState()
+
+    useEffect(() => {
+        console.log(router.query)
+        if (!router || !router.query || !router.query.payTo || !router.query.amount) {
+            return
+        }
+        const {payTo, message, amount} = router.query
+        setInputPayTo(payTo)
+        setInputMessage(message)
+        setInputAmount(amount)
+    }, [router])
     return (
         <div>
             <h1>TX PREVIEW</h1>
+            <p>PayTo: {inputPayTo}</p>
+            <p>Message: {inputMessage}</p>
+            <p>Amount: {inputAmount}</p>
         </div>
     )
 }
