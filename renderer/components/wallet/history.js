8a887db752ea3a67b82609c52b3277b4ba636f4d
Jason Chavannes <jason.chavannes@gmail.com>
2022-02-24 01:20:46 +0000

    Include balance in tx history list

--- renderer/components/wallet/history.js
+++ renderer/components/wallet/history.js
@@ -5,14 +5,20 @@ const History = () => {
     const [txs, setTxs] = useState([])
     useEffect(async () => {
         let wallet = await GetWallet()
-        setTxs(await window.electron.getTransactions(wallet.addresses))
+        let txs = await window.electron.getTransactions(wallet.addresses)
+        let balance = 0
+        for (let i = txs.length - 1; i >= 0; i--) {
+            balance += txs[i].value
+            txs[i].balance = balance
+        }
+        setTxs(txs)
     }, [])
     return (
         <div>
             {!txs.length && <p>No transactions found</p>}
             {txs.map((tx, i) => {
                 return (
-                    <p key={i}>{i}: {tx.hash} - {tx.timestamp} - {tx.value}</p>
+                    <p key={i}>{i}: {tx.hash} - {tx.timestamp} - {tx.value} - {tx.balance}</p>
                 )
             })}
         </div>
