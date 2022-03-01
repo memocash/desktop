63a20685dff25b4474711494200218fe5894eb33
Jason Chavannes <jason.chavannes@gmail.com>
2022-03-01 00:11:01 +0000

    Display coins.

--- main/data/outputs.js
+++ main/data/outputs.js
@@ -2,9 +2,12 @@ const {Select} = require("./sqlite");
 const GetCoins = (addresses) => {
     const query = "" +
         "SELECT " +
-        "   outputs.* " +
+        "   outputs.*, " +
+        "   blocks.height " +
         "FROM outputs " +
         "LEFT JOIN inputs ON (inputs.prev_hash = outputs.hash AND inputs.prev_index = outputs.`index`) " +
+        "LEFT JOIN block_txs ON (block_txs.tx_hash = outputs.hash) " +
+        "LEFT JOIN blocks ON (blocks.hash = block_txs.block_hash) " +
         "WHERE outputs.address IN (" + Array(addresses.length).fill("?").join(", ") + ") " +
         "AND inputs.hash IS NULL " +
         "GROUP BY outputs.hash, outputs.`index` "
