3cfedcef837d7670c3f84bda64b9e889acd248d3
Jason Chavannes <jason.chavannes@gmail.com>
2022-03-02 00:42:21 -0800

    Remember state of previous wallet tabs.

--- renderer/pages/wallet.js
+++ renderer/pages/wallet.js
@@ -1,6 +1,6 @@
 import Frame, {Tabs} from "../components/wallet/frame";
 import Addresses from "../components/wallet/addresses";
-import {useEffect, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import History from "../components/wallet/history";
 import Send from "../components/wallet/send";
 import Receive from "../components/wallet/receive";
@@ -9,25 +9,47 @@ import Update from "../components/wallet/update";
 
 const StorageKeyWalletTab = "wallet-tab"
 
+const Page = ({tab, page, shown, children}) => {
+    const includePage = tab === page || shown.current.includes(page)
+    const style = {}
+    if (includePage && tab !== page) {
+        style.display = "none"
+    }
+    return (
+        <>
+            {includePage && (
+                <div style={style}>
+                    {children}
+                </div>
+            )}
+        </>
+    )
+}
+
 const WalletLoaded = () => {
     const [tab, setTab] = useState("")
     const [connected, setConnected] = useState(false)
+    const shownRef = useRef([])
     useEffect(async () => {
         const tab = await window.electron.getWindowStorage(StorageKeyWalletTab) || Tabs.History
         setTab(tab)
+        shownRef.current.push(tab)
     }, [])
     const handleClicked = (tab) => {
         setTab(tab)
         window.electron.setWindowStorage(StorageKeyWalletTab, tab)
+        if (!shownRef.current.includes(tab)) {
+            shownRef.current.push(tab)
+        }
     }
     return (
         <>
             <Frame selected={tab} clicked={handleClicked} connected={connected}>
-                {tab === Tabs.History && <History/>}
-                {tab === Tabs.Send && <Send/>}
-                {tab === Tabs.Receive && <Receive/>}
-                {tab === Tabs.Addresses && <Addresses/>}
-                {tab === Tabs.Coins && <Coins/>}
+                <Page tab={tab} page={Tabs.History} shown={shownRef}><History/></Page>
+                <Page tab={tab} page={Tabs.Send} shown={shownRef}><Send/></Page>
+                <Page tab={tab} page={Tabs.Receive} shown={shownRef}><Receive/></Page>
+                <Page tab={tab} page={Tabs.Addresses} shown={shownRef}><Addresses/></Page>
+                <Page tab={tab} page={Tabs.Coins} shown={shownRef}><Coins/></Page>
             </Frame>
             <Update setConnected={setConnected}/>
         </>
