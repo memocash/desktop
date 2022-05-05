6b91369c27b5dded8803a6dc30c99abb2d3a175c
Jason Chavannes <jason.chavannes@gmail.com>
2022-05-04 22:56:59 -0700

    Subscribe to full tx info and save/update on new tx

--- renderer/components/wallet/update.js
+++ renderer/components/wallet/update.js
@@ -94,11 +94,34 @@ const Update = ({setConnected, setLastUpdate}) => {
                 hash
                 seen
                 raw
+                inputs {
+                    index
+                    prev_hash
+                    prev_index
+                }
+                outputs {
+                    index
+                    amount
+                    lock {
+                        address
+                    }
+                }
+                blocks {
+                    hash
+                    timestamp
+                    height
+                }
             }
         }
         `
-        window.electron.listenNewTxs(query, {address: wallet.addresses[0]}, (tx) => {
+        window.electron.listenNewTxs(query, {address: wallet.addresses[0]}, async (tx) => {
             console.log(tx)
+            await window.electron.saveTransactions([tx.address])
+            await window.electron.generateHistory(wallet.addresses)
+            if (typeof setLastUpdate === "function") {
+                console.log("setting new last update: " + (new Date()).toISOString())
+                setLastUpdate((new Date()).toISOString())
+            }
         })
     }, [])
     const loadOutputs = async ({addresses}) => {
