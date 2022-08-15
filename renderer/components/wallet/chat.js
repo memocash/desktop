66515e8f81be807b07c8e5d4bc84594ecb4c7434
Jason Chavannes <jason.chavannes@gmail.com>
2022-08-14 18:22:45 -0700

    Beginning of chat styling

--- renderer/components/wallet/chat.js
+++ renderer/components/wallet/chat.js
@@ -1,7 +1,7 @@
-import profile from "../../styles/profile.module.css";
 import {useEffect, useState} from "react";
-import UpdateChat from "./update/chat";
+import styles from "../../styles/chat.module.css";
 import GetWallet from "../util/wallet";
+import UpdateChat from "./update/chat";
 
 const Chat = () => {
     const [lastUpdate, setLastUpdate] = useState(null);
@@ -13,19 +13,28 @@ const Chat = () => {
     useEffect(async () => {
         const userAddresses = (await GetWallet()).addresses
         const posts = await window.electron.getChatPosts({room, userAddresses})
+        posts.reverse()
         setPosts(posts)
     }, [lastUpdate])
     return (
-        <div className={profile.wrapper}>
-            <h4>Chat room: <i>{room}</i></h4>
-            {posts.map((post, index) => {
-                return (
-                    <div key={index}>
-                        <div>{post.tx_hash}</div>
-                        <div>{post.text}</div>
-                    </div>
-                )
-            })}
+        <div className={styles.wrapper}>
+            <div className={styles.sidebar}>
+                <div className={styles.sidebarHeader}>
+                    <h2>{room}</h2>
+                </div>
+            </div>
+            <div className={styles.content}>
+                <div className={styles.posts}>
+                    {posts.map((post, index) => {
+                        return (
+                            <div key={index} className={styles.post}>
+                                <div>{post.tx_hash}</div>
+                                <div>{post.text}</div>
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
         </div>
     )
 }
