88cc484181b470b89f78d2f26742e88b7ccff3e9
Jason Chavannes <jason.chavannes@gmail.com>
2022-08-18 21:39:13 -0700

    Highlight room in list if currently open

--- renderer/components/wallet/chat.js
+++ renderer/components/wallet/chat.js
@@ -139,9 +139,12 @@ const Chat = ({setModal}) => {
                     </form>
                 </div>
                 <div className={styles.sidebar_content}>
-                    <ul>{follows.map((follow, i) => (<li key={i} onClick={(e) => clickRoom(e, follow.room)}>
-                        {follow.room}
-                    </li>))}</ul>
+                    <ul>{follows.map((follow, i) => (
+                        <li key={i} onClick={(e) => clickRoom(e, follow.room)}
+                            className={room === follow.room ? styles.selected : ""}>
+                            {follow.room}
+                        </li>
+                    ))}</ul>
                 </div>
                 <div className={styles.sidebar_footer}>
                 </div>
